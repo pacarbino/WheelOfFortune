9df8bcf6299a3b755b484d7e4869f2524adf9d73
pacarbino <filosophy@gmail.com>
2022-04-08 16:14:45 -0500

    updated some notes from eod review

--- PseudoCode.py
+++ PseudoCode.py
@@ -4,21 +4,25 @@
 # rules:
 # rounds 1 and 2:
 
-## create list of words to choose from
-## create list of wheel spaces to choose from
+## create list of words to choose from (CSV file not necessary. text file accepltable)
+## create list of wheel spaces to choose from (19 spots, not 24. 100-900 in $50 increments, plus lose a turn, plus bankrupt)
 ## create blanks (gameboard) list
 ## create letters guessed list
+## create random mystery prize list (or, you can just do a flat ammount)
 
-## create bank for player 1
-## create bank for player 2
-## create bank for player 3
+## create perm bank for player 1
+## create perm bank for player 2
+## create perm bank for player 3
 
 ## create random word generator using word list
+    ## creates word to be guessed
+    ## appends blanks (gameboard) list
 ## create random wheel space generator from list
+## create random mystery prize generator from list
 
 # puzzle of letters presented:
     ## call random word generator
-    ## print display of word in "blanks" list
+    ## print display of word in blanks (gameboard)  list
 
 # 3 players
 # everyone starts with $0
@@ -35,26 +39,30 @@
                     ## solve
                 #if bank > 0:
                     ## spin
-                    ## solve 
                     ## buy a vowel
+                    ## solve 
 
             # 1) spin 
                 ## call random wheel space generator
                     ## "spin" = result of function call above
                     ## if spin = prize:
                         ## ask for user input regarding letter choice
-                        ## check to see if letter is in puzzle:
+                        ## check to see if letter is in guessed letters list:
                             ## if yes:
-                                ## player bank + prize*[number of occurances of letter in word]
-                                ## append words guessed list
-                                ## append blanks (gameboard) list
-                                ## get input regarding whether player wnats to:
-                                    ## spin
-                                    ## buy a vowel
-                                    ## solve
+                                ## guess again (repeat get input)
                             ## if no: 
-                                ## append words guessed list
-                                ## player's turn ends (move to next player)
+                                ## check to see if letter is in puzzle:
+                                    ## if yes:
+                                        ## player bank + prize*[number of occurances of letter in word]
+                                        ## append words guessed list
+                                        ## append blanks (gameboard) list
+                                        ## get input regarding whether player wnats to:
+                                            ## spin
+                                            ## buy a vowel
+                                            ## solve
+                                    ## if no: 
+                                        ## append letters guessed list
+                                        ## player's turn ends (move to next player)
 
             # 2) buy a vowel
                 ## player bank - 250
@@ -67,6 +75,9 @@
                                 ## spin
                                 ## buy a vowel
                                 ## solve
+                        ## else: 
+                            ## append letters guessed list
+                            ## player's turn ends (move to next player)
 
             # 3) solve puzzle
                 ## get user input regarding guess:
@@ -86,12 +97,24 @@
 # round 3:
 
 # player picks a mystery prize
+## call random mystery prize generator
+
 # puzzle presented:
+## call random word generator
+## 
+
 # default free letters given [r,s,t,l,n,e]
     # if letters above are in puzzle, they're shown in place
+    ## append blanks (gameboard) list)
+
 # player picks 3 more consonants and one more vowel
+## get player input regarding choices
     # if letters above are in puzzle, they're shown in place
+    ## append blanks (gameboard) list)
+
 # player guesses puzzle (one chance)
+## get player input regarding choice
+## check to see if input (guess) == word
     # if right, player wins mystery prize
     # if incorrect, player does not win mystery prize
 
